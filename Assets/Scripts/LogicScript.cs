c23539053e25b7edbda2734a7a5a464d0406888b
LEPruitt <legomaster48@outlook.com>
2023-05-17 17:42:29 -0500

    Quality-of-life Improvements
    Better UI and a high score system. Tell me what you think!

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
-    public Text scoreText;
+    public Text scoreText, highScoreText;
     public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
@@ -15,6 +15,19 @@ public class LogicScript : MonoBehaviour
     {
         playerScore += scoreToAdd;
         scoreText.text = playerScore.ToString();
+        CheckHighScore(); //Calls to check if current score is higher than high score;
+    }
+    public void CheckHighScore() //Manages the high score;
+    {
+        if (playerScore > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", playerScore); //PlayerPrefs can be used to permantently store values;
+            highScoreText.text = playerScore.ToString();
+        }
+    }
+    public void Start()
+    {
+        highScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
     }
 
     public void restartGame()
@@ -25,5 +38,11 @@ public class LogicScript : MonoBehaviour
     public void gameOver()
     {
         gameOverScreen.SetActive(true);
+    }
+
+    public void ResetScore()
+    {
+        PlayerPrefs.SetInt("HighScore", 0); //Resets the high score;
+        highScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
     }
 }
